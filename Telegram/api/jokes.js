8ed4cc14735a2a40b69cfba732dc07e7b1cc14ed
victorsantos-jobs <152844642+victorsantos-jobs@users.noreply.github.com>
2023-12-12 11:19:54 -0300

    Add files via upload

---
+++ Telegram/api/jokes.js
@@ -0,0 +1,49 @@
+
+
+
+var a1 = '- Qual é a panela que está sempre triste?- A panela depressão.'
+var a2 = 'Qual é a fórmula da água benta?R.: H Deus O!'
+var a3 = '- Por que as plantinhas não falam?- Porque elas são mudinhas.'
+var a4 = '- Qual a cidade brasileira que não possui táxis?- Uberlândia.'
+
+var a5 = 'Piada 1'
+var a6 = 'piada 2'
+var a7 = ' piada 3'
+var a8 = 'piada 4'
+
+var a9 = 'Qual o café que está sempre estressado?R: O caputinho'
+var a10 = 'piada 5'
+var a11 = 'piada 6'
+var a12 = 'O que o tomate foi fazer no banco?R: Foi tirar extrato.'
+
+const jokes = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12];
+
+
+var a = console.log("Está rodando o bot;")
+
+
+
+module.exports = jokes;
+
+
+// chat: {
+//     id: 125394439,
+//     first_name: 'Mariboy',
+//     last_name: '(Vulgo Mariano)',
+//     username: 'O_Mariboy',
+//     type: 'private'
+//   },
+//   date: 1701438507,
+//   text: 'piada'
+
+
+// chat: {
+//     id: 799312676,
+//     first_name: 'Claudio',
+//     last_name: 'Lima',
+//     username: 'CSL00Q',
+//     type: 'private'
+//   },
+//   date: 1701459476,
+//   text: 'Forte'
+// }
