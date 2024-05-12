9503e36c2ae1717add74a8c1b71ecaf5f999daa3
VICTOR SANTOS <152844642+victorsantos-jobs@users.noreply.github.com>
2024-05-12 07:56:20 -0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -9,6 +9,11 @@ app.get('/inicio', (req, res) => {
     res.send('Texto que vai aparecer na tela.');
 });
 
+app.get('/', (req, res) => {
+    res.send('Rota principal, fiz essa e a rota inicio.');
+});
+
+
 app.get('/meio', (req, res) => {
     res.send({nome:"Pedro",
 idade: 25,
